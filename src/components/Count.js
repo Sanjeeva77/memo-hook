427e509564c60225e3687de52ce671c28bedfd98
Sanjeeva <sanjeevapottabathini7729@gmail.com>
2023-08-01 12:50:44 +0530

    memo

---
+++ src/components/Count.js
@@ -0,0 +1,36 @@
+import React,{useMemo,useState} from 'react'
+
+export const Count = () => {
+    const [counterOne,setCounterOne]=useState(0)
+    const [counterTwo,setCounterTwo]=useState(0)
+    const isEven=useMemo(()=>{
+        let i=0
+        while(i<2000000000)i++
+        return counterOne%2===0
+    },[counterOne])
+
+    // const isEven=()=>{
+    //     let i=0
+    //     while(i<20000000000)i++
+    //     return counterOne%2===0
+    // }
+
+    const incrementOne=()=>{
+        setCounterOne(counterOne+1)
+    }
+    const incrementTwo=()=>{
+        setCounterTwo(counterTwo+1)
+    }
+    return (
+        <div>
+        <div>
+            <button onClick={incrementOne}>Count One - {counterOne}</button>
+            <span>{isEven?'Even':'Odd'}</span>
+        </div>
+        <div>
+            <button onClick={incrementTwo}>Count Two - {counterTwo}</button>
+        </div>
+        </div>
+           
+    )
+}
